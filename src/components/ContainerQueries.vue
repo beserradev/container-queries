98c6cb5c324156f4acd20a2d1f03f8d20baf7a58
beserradev <40183628+beserradev@users.noreply.github.com>
2021-10-05 14:44:13 -0300

    Fim do curso

---
+++ src/components/ContainerQueries.vue
@@ -0,0 +1,126 @@
+<template>
+  <div class="main">
+    <section class="section">
+      <h1>Container Queries</h1>
+      <h3>"Agora não dá, na volta a gente compra" - Será?</h3>
+      <h4>Porque esse hype todo?</h4>
+      <p>
+        <a
+          href="https://ishadeed.com/article/say-hello-to-css-container-queries/"
+          >Sobre Container Queries</a
+        >
+      </p>
+    </section>
+    <section class="section">
+      <ul>
+        <h4>
+          Container Queries virão para revolucionar o jeito de fazer interfaces
+          responsivas(?)
+        </h4>
+        <li class="item">
+          Podemos definir as caracateristícas de um elemento filho de acordo com
+          o tamanho do elemento pai
+          <div>
+            <h5>Veja só essa aplicação</h5>
+            <img
+              src="https://ishadeed.com/assets/cq/intro.jpg"
+              class="img__exemplo"
+              aria="Qual a diferença?"
+            />
+            <h6>
+              Basicamente, antes um media query era determinado pelo tamanho da
+              View Port. Agora isso não precisa se reestringir somente a isso.
+            </h6>
+          </div>
+        </li>
+        <li class="item">
+          Você pode ter o seu "próprio bootstrap" só que melhor!
+          <div>
+            <h5></h5>
+            <img
+              src="../assets/codigo1.png"
+              class="img__exemplo"
+              aria="Setup Simples"
+            />
+            <h4>
+              Uma vez que temos a definição de tamanho do pai do elemento, temos
+              uma contextualização de componente muito melhor
+            </h4>
+          </div>
+        </li>
+        <li class="item">
+          <div>
+            <h2>Houston We have a problem:</h2>
+            <h5>Vamos olhas o Can I Use?</h5>
+            <a href="https://caniuse.com/css-container-queries"
+              >Can I Use /?css="Container Queries"?</a
+            >
+            <h6>Triste, complicado... Como tudo em CSS, como na vida</h6>
+          </div>
+        </li>
+        <li class="item">
+          <div>
+            <h5>
+              Ok... É triste mesmo, e também muito Legal. Mas se não funciona em
+              nada ainda, porque você ta mostrando isso?
+            </h5>
+            <h2>
+              Porque é <strong>possível</strong> fazer coisas parecidas e usar
+              como <strong>Fallback no futuro</strong>
+            </h2>
+          </div>
+        </li>
+      </ul>
+    </section>
+    <section class="section">
+      <h2>Direto ao assunto</h2>
+      <div style="margin: 2em 0">
+        <TelaPadrao />
+      </div>
+    </section>
+  </div>
+</template>
+
+<script>
+import TelaPadrao from "./TelaPadrao.vue";
+
+export default {
+  name: "ContainerQueries",
+  components: {
+    TelaPadrao,
+  },
+};
+</script>
+<style scoped lang="scss">
+.section {
+  display: flex;
+  flex: 0 0 100%;
+  justify-items: center;
+  flex-direction: column;
+  background: #ffffff;
+  border: 1px solid #dadce0;
+  -moz-border-radius: 8px;
+  border-radius: 8px;
+  position: relative;
+  margin: 1em 0 1em 1em;
+  padding: 1em;
+
+  .item {
+    width: 100%;
+  }
+  .img__exemplo,
+  .img__prototipo {
+    max-width: 50%;
+  }
+}
+h3 {
+  margin: 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+a {
+  color: #42b983;
+}
+</style>
---
+++ src/components/TelaPadrao.vue
@@ -0,0 +1,176 @@
+<template>
+  <div class="main">
+    <h2>Solução 1 - CssGrid</h2>
+    <div class="cards var-grid">
+      <div class="card" style="--grid-min: 150px">
+        <div class="img"><div class="img__exemplo"></div></div>
+        <div class="titulo">
+          <h3>Titulo do card</h3>
+        </div>
+        <div class="conteudo">
+          <p>Aqui temos um resumo do conteudo do assunto do card</p>
+        </div>
+      </div>
+      <div class="card">
+        <div class="img"><div class="img__exemplo"></div></div>
+        <div class="titulo">
+          <h3>Titulo do card 2</h3>
+        </div>
+        <div class="conteudo">
+          <p>Aqui temos um resumo do conteudo do assunto do card</p>
+        </div>
+      </div>
+      <div class="card">
+        <div class="img"><div class="img__exemplo"></div></div>
+        <div class="titulo">
+          <h3>Titulo do card 2</h3>
+        </div>
+        <div class="conteudo">
+          <p>Aqui temos um resumo do conteudo do assunto do card 3</p>
+        </div>
+      </div>
+    </div>
+
+    <h2>Solução 2 - FlexBox</h2>
+    <div class="cards var-flex">
+      <div class="card var-flex" style="--flex-min: 150px;">
+        <div class="img"><div class="img__exemplo"></div></div>
+        <div class="titulo">
+          <h3>Titulo do card</h3>
+        </div>
+        <div class="conteudo">
+          <p>Aqui temos um resumo do conteudo do assunto do card</p>
+        </div>
+      </div>
+      <div class="card">
+        <div class="img"><div class="img__exemplo"></div></div>
+        <div class="titulo">
+          <h3>Titulo do card 2</h3>
+        </div>
+        <div class="conteudo">
+          <p>Aqui temos um resumo do conteudo do assunto do card</p>
+        </div>
+      </div>
+      <div class="card">
+        <div class="img"><div class="img__exemplo"></div></div>
+        <div class="titulo">
+          <h3>Titulo do card 2</h3>
+        </div>
+        <div class="conteudo">
+          <p>Aqui temos um resumo do conteudo do assunto do card 3</p>
+        </div>
+      </div>
+    </div>
+
+    <h2>Solução 3 - FlexBox-2</h2>
+    <div class="cards var-flex2">
+      <div class="card var-flex2" style="--flex2-min: 150px;">
+        <div class="img"><div class="img__exemplo"></div></div>
+        <div class="titulo">
+          <h3>Titulo do card</h3>
+        </div>
+        <div class="conteudo">
+          <p>Aqui temos um resumo do conteudo do assunto do card</p>
+        </div>
+      </div>
+      <div class="card var-flex2" style="--flex2-min: 80px;">
+        <div class="img"><div class="img__exemplo"></div></div>
+        <div class="titulo">
+          <h3>Titulo do card 2</h3>
+        </div>
+        <div class="conteudo">
+          <p>Aqui temos um resumo do conteudo do assunto do card</p>
+        </div>
+      </div>
+      <div class="card var-flex2">
+        <div class="img"><div class="img__exemplo"></div></div>
+        <div class="titulo">
+          <h3>Titulo do card 3</h3>
+        </div>
+        <div class="conteudo">
+          <p>Aqui temos um resumo do conteudo do assunto do card 3</p>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TelaPadrao",
+};
+</script>
+
+<style lang="scss">
+:root {
+  --grid-min: 300px;
+  --flex-min: 300px;
+  --flex2-min: 300px;
+}
+
+.main {
+  grid-area: main;
+  background: #f9f9f9;
+  padding: 3rem 1rem;
+}
+
+.var-grid {
+  display: grid;
+  grid-gap: 1rem;
+  grid-template-columns: repeat(auto-fit, minmax(var(--grid-min), 1fr));
+}
+
+.var-flex {
+  display: flex;
+  flex-wrap: wrap;
+  margin: 1rem -0.5rem;
+
+  > div {
+    flex-grow: 1;
+    flex-basis: calc((var(--flex-min) - 100%) * 999);
+    margin: 0.5rem;
+
+    &.flex--auto {
+      flex: 0 1 auto;
+    }
+  	}
+}
+
+.var-flex2 {
+  display: flex;
+  flex-wrap: wrap;
+  margin: 1rem -0.5rem;
+
+  > * {
+    flex: 1 1 var(--flex2-min);
+    margin: 0.5rem;
+    
+    &.flex--auto {
+      flex: 0 1 auto;
+    }
+  }
+}
+
+.cards {
+  padding: 1em;
+  &.var-grid {
+    border: 1px solid limegreen;
+  }
+
+  .card {
+    border: 1px solid #dadce0;
+    -moz-border-radius: 8px;
+    border-radius: 8px;
+    min-width: 20vh;
+  }
+}
+
+.img {
+  .img__exemplo {
+    background-color: grey;
+    width: 50px;
+    height: 50px;
+    margin: 0.5em;
+  }
+}
+</style>
